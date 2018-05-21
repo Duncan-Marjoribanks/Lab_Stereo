6b51abc064946d2941ade3346964343b58ed5e76
sarahmurphy86 <sarah.graham86@gmail.com>
2018-05-21 13:59:51 +0100

    CD Player class set up and test passed

--- src/main/java/CDPlayer.java
+++ src/main/java/CDPlayer.java
@@ -1,2 +1,10 @@
-public class CDPlayer {
+public class CDPlayer extends Component {
+
+    public CDPlayer(String make, String model){
+        super(make, model);
+    }
+
+    public String play(String song){
+        return "Playing " + song;
+    }
 }
--- src/test/java/CDPlayerTest.java
+++ src/test/java/CDPlayerTest.java
@@ -1,2 +1,32 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class CDPlayerTest {
+
+    CDPlayer cDPlayer;
+
+    @Before
+    public void before(){
+        cDPlayer = new CDPlayer("Sony", "Walkman");
+    }
+
+    @Test
+    public void canPlaySong(){
+        String result = cDPlayer.play("Britney Spears");
+        assertEquals("Playing Britney Spears", result);
+    }
+
+    @Test
+    public void hasMake(){
+        assertEquals("Sony", cDPlayer.hasMake());
+    }
+
+    @Test
+    public void hasModel(){
+        assertEquals("Walkman", cDPlayer.hasModel());
+    }
+
+
 }
